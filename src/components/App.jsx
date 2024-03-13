b8374b595227bbc9f9edd4af898750093b3c4ce3
Chad Schroeder <77646640+calansc@users.noreply.github.com>
2024-03-13 14:36:19 -0400

    Create character shuffler.Add score and high score tracker. Add click handler to track picks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,57 @@
-import Card from "./Card.jsx"
+import { useState, useEffect } from 'react';
 import characters from "./Characters.jsx"
 
+
 function App() {
+    const [score, setScore] = useState(0);
+    const [highScore, setHighScore] = useState(0);
+    const [newCharacters, setNewCharacters] = useState([]);
+    const [pickList, setPickList] = useState([]);
+
+    useEffect(() => {
+        if (score > highScore) {
+            setHighScore(score)
+        }
+    },[highScore, score]
+    );
+
+    const shuffle = arr => [...arr].sort(() => Math.random() - 0.5);
+
+    useEffect(() => {
+        setNewCharacters(shuffle(characters))
+    },[score]);
+
+    function handleClick(e) {
+        setScore(score + 1);
+        if (pickList.includes(e.target.id)) {
+            // Lose
+            alert('You Lose, Try Again!');
+            setScore(0);
+            setPickList([]);
+        } else {
+            setPickList([...pickList, e.target.id])
+        }
+
+    }
+
+    console.log(newCharacters);
+    console.log(pickList);
+
+
     return (<div>
+        <div className="score">Score:{score}</div>
+        <div className="score">High Score:{highScore}</div>
         <div className="cards">
-            {characters.map((character) => (
-               <div className="character-card" key={character.id}> 
-                    <img className="character-image" 
+            {newCharacters.map((character) => (
+                <div className="character-card" 
+                id={character.id} 
+                key={character.id} 
+                onClick={(e) => handleClick(e)}>
+                    <img className="character-image"
+                        id={character.id} 
                         src={character.img}
                     />
-                    <div className="character-name">
+                    <div className="character-name" id={character.id}>
                         {character.name}
                     </div>
                 </div>
